258a7e8eaab90814909a833664630ec1e7f7ec23
Croaker <72915331+MikZmu@users.noreply.github.com>
2025-04-16 19:47:30 +0200

    standarizing

--- clicker/MLP_playground.py
+++ clicker/MLP_playground.py
@@ -4,14 +4,16 @@ import MLP_class
 import  getData_class
 import training_class
 from torch.optim.lr_scheduler import ReduceLROnPlateau
+import get_Standarized_data
 
 model = MLP_class.MLP(input_size=7, output_size=1, hidden_layers=[512, 512, 512, 512, 512])
 print(model)
-data = getData_class.getData(['C'])
-print(data)
+#data = getData_class.getData(['C'])
+standarized_data = get_Standarized_data.get_Standarized_data("cScaler",'C')
+print(standarized_data)
 loss = nn.MSELoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 #scheduler = ReduceLROnPlateau(optimizer, mode='min', factor=0.9, patience=10)
 torch.cuda.empty_cache()
-training_class.train_model(model, loss, optimizer, x_train=data[0], y_train=data[1], x_test=data[2].squeeze(), y_test=data[3], save_path='modelC_512_512_512_512_512.pth')
+training_class.train_model(model, loss, optimizer, x_train=standarized_data[0], y_train=standarized_data[1], x_test=standarized_data[2].squeeze(), y_test=standarized_data[3], save_path='modelCstandard_128_64_64_32.pth')
 
